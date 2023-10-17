3a36057b875ed0118e967a6ab80d3aa79399a27a
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-10-17 15:18:10 -0400

    Update nba_players.py

--- nba_players.py
+++ nba_players.py
@@ -6,11 +6,11 @@ import csv
 # Set URL for NBA API
 url = "https://free-nba.p.rapidapi.com/players"
 
-# Input for NBA API Key
+# User input for NBA API Key and set it to a variable
 api_key = input(f"Please enter your API Key for {url}:")
 
 def nba_player_data():
-	    # Set API credentials to access the free-nba API 
+	# Set API credentials to access the free-nba API 
     headers = {
         "X-RapidAPI-Key": api_key ,
         "X-RapidAPI-Host": "free-nba.p.rapidapi.com"
@@ -25,12 +25,12 @@ def nba_player_data():
 
     # Get the total number of pages in the API response
     total_pages = response.json()["meta"]["total_pages"]
-    # print(response.json())
 
     # Open a CSV file to write the API response
     with open("nba.csv", "w") as file:
+        #Create a writer object variable to write into the csv
         writer = csv.writer(file)
-        # Loop through each page in the API response
+        #Loop through each page in the API response
         for page in range(total_pages):
             print("Writing to nba.csv")
             # For each row parse through the json formatted string into Python object 
@@ -38,21 +38,31 @@ def nba_player_data():
                 # and then write player data to a row in the CSV
                 writer.writerow([row["id"], row["first_name"] + " " + row["last_name"], row["position"], row["team"]["full_name"]])
 
+#While this is true, continue looping
 while True:
     try:
+        #Run function
         nba_player_data()
 
+    #Error handling for incorrect api key
     except(KeyError):
         print("Invalid API Key")
+        #Set user response to if they want to re-enter their API key
         users_response = input("Would you like to re-enter your API Key (Enter 'yes' or 'no' ONLY)? ")
+        #If the user response is "no", do this:
         if users_response.lower() == "no":
             print("Exiting...")
             exit()
+        #If user response is "yes", do this:
         elif users_response.lower() == "yes":
+            #ask for user api key and set it to a variable
             api_key = input(f"Please enter your API Key for {url}:")
+            #run function
             nba_player_data()
+        #until a valid response if given
         else:
-            continue  # Continue the loop to ask for a valid response
+            #continue looping through the if statement 
+            continue 
     else:
-        # The code inside the try block was successful, so exit the loop
+        # Exit the while loop
         break

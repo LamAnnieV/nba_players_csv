013f3ad68eaf984630c76ff0f6af81ee7edc8e8b
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-10-17 12:41:48 -0400

    Update nba_players.py

--- nba_players.py
+++ nba_players.py
@@ -16,8 +16,8 @@ headers = {
 # Create a variable for the page parameter that would be passed later to the GET request
 page_parameters = {"page":"0","per_page":"1000"}
 
-######################################################################
-# Make a GET request to API URL, passing in the parameters and setting it to a reponse to be use later
+
+# Make a GET request to API URL, passing in the parameters and setting it to a response to be use later
 response = requests.get(url, headers=headers, params=page_parameters)                                               #, params=querystring
 
 # Get the total number of pages in the API response
@@ -29,7 +29,7 @@ with open("nba.csv", "w") as file:
     # Loop through each page in the API response
     for page in range(total_pages):
         print("Writing to nba.csv")
-        # For each row parse through the json fromatted string into python object 
+        # For each row parse through the json formatted string into Python object 
         for row in response.json()["data"]:
             # and then write player data to a row in the CSV
             writer.writerow([row["id"], row["first_name"] + " " + row["last_name"], row["position"], row["team"]["full_name"]]) 
